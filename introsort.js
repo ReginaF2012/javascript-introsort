a2e9b94b11f385aa3c86686e2b33c8ed5a529155
ReginaF2012 <skritschy@gmail.com>
2021-02-07 14:20:08 -0600

    insertion sort

--- introsort.js
+++ introsort.js
@@ -2,3 +2,19 @@
 function swap(array, index1, index2) {
     [[array[index1], array[index2]] = [array[index2], array[index1]]];
 }
+
+function insertionSort(arr, start = 0, end = arr.length - 1) {
+    let left = start;
+    let right = end;
+
+    for (let i = left + 1; i <= right; i++) {
+        let key = arr[i];
+        let j = i - 1;
+
+        while (j >= start && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
