588346c97fa9c3859ff91ffbe984298f15c8aa31
ReginaF2012 <skritschy@gmail.com>
2021-02-07 15:23:12 -0600

    hoare's partition

--- introsort.js
+++ introsort.js
@@ -18,3 +18,30 @@ function insertionSort(arr, start = 0, end = arr.length - 1) {
         arr[j + 1] = key;
     }
 }
+
+  
+function partition(arr, start, end) {
+    // For random pivot use => arr[Math.floor(Math.random() * (end - start + 1) + start)];
+    const pivotVal = arr[Math.floor((start + end) / 2)];
+
+    while (start <= end) {
+        while (arr[start] < pivotVal) {
+            start++;
+        }
+        while (arr[end] > pivotVal) {
+            end--;
+        }
+
+        if (start <= end) {
+            // swap
+            let temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    return start;
+}
